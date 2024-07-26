4122e479098b024a96b6095fd9e5b9728239250a
eqwert <yevgeniy.novikov.2003@inbox.ru>
2024-07-26 18:30:48 +0500

    added image in models

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     # http://127.0.0.1:8000/admin/
     path("admin/", admin.site.urls),
-
     # http://127.0.0.1:8000/myapp/
-    path("myapp/", include("myapp.urls", namespace="myapp"))
-
+    path("myapp/", include("myapp.urls", namespace="myapp")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
