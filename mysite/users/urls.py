44920e7bfad75ce8f2466d0be97efdcc05eec245
eqwert <yevgeniy.novikov.2003@inbox.ru>
2024-08-15 18:07:59 +0500

    creat login logout

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import register
+from .views import register, profile
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = "users"  # namespace
 
 
 urlpatterns = [
-    # http://127.0.0.1:8000/users/
+    # http://127.0.0.1:8000/users/register/
     path("register/", register, name="register"),
+    path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    path("profile/", profile, name="profile"),
 ]
